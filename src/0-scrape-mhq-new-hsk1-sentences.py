b6ba92520d9179458bfec0fcf56ee8fdbd478613
Rosie Le Faive <lefaive@gmail.com>
2026-07-11 18:49:36 -0300

    Add 500-word dictionary and do smarter splitting.

--- src/0-scrape-mhq-new-hsk1-sentences.py
+++ src/0-scrape-mhq-new-hsk1-sentences.py
@@ -23,3 +23,22 @@ with open("../vendor/mhq-new-hsk1-sentences.html") as fp:
         
 with open('../data/parsed-hsk1-sentences.json', 'w', encoding='utf8') as fp:
     json.dump(data, fp, ensure_ascii=False)
+
+
+# Import HSK1 "3.0" 2022 wordlist of 500 words.
+data = {}
+with open("../vendor/mhq-500-word-list.html") as fp:
+    soup = BeautifulSoup(fp, 'lxml')
+    table = soup.find('table')
+    for row in table.find_all('tr'):
+        chinese = row.td.next_sibling.text
+        pinyin = row.td.next_sibling.next_sibling.text
+        english = row.td.next_sibling.next_sibling.next_sibling.text
+        data[chinese] = {
+            'translations': {
+                'en': english
+            },
+            'pinyin': pinyin
+        }
+with open('../data/hsk1-500-dict.json', 'w', encoding='utf8') as fp:
+    json.dump(data, fp, ensure_ascii=False, indent=4)
--- src/1-split-and-add-definitions.py
+++ src/1-split-and-add-definitions.py
@@ -6,8 +6,10 @@ import jieba
 definition_files = [
     'vendor/hsk-words/2010/hsk2-words.json', 
     'vendor/hsk-words/2010/hsk1-words.json', 
-    'vendor/hsk-words/2026/hsk2-words.json', 
-    'vendor/hsk-words/2026/hsk1-words.json']
+    'vendor/hsk-words/2026/hsk2-words.json',
+    'data/hsk1-500-dict.json',
+    'vendor/hsk-words/2026/hsk1-words.json'
+    ]
 definitions = {}
 
 for f in definition_files:
@@ -15,7 +17,33 @@ for f in definition_files:
         definition_set = json.load(fp)
         definitions.update(definition_set)
 
-#print(definitions.keys())
+
+def get_def_for_word(word, definitions):
+    word = word.strip()
+    if word in ['？', '。', '！', ',']:
+        return []
+    
+    result = []
+    if word not in definitions.keys():
+        resolved = False
+        if len(word) == 4:
+            subwords = [word[:2], word[2:]]
+            for subword in subwords:
+                result.extend(get_def_for_word(subword, definitions))
+        if len(word) >= 3:
+            subwords = [word[0], word[:-1],  word[1:], word[-1]]
+            for subword in subwords:
+                result.extend(get_def_for_word(subword, definitions))
+        if len(word) == 2:
+            for subword in word:
+                result.extend(get_def_for_word(subword, definitions))  
+    else:
+        result.append({
+            'zh': word,
+            'en': definitions[word]['translations']['en']
+        })
+    return result
+    
 
 source_file = 'data/parsed-hsk1-sentences.json'
 with open(source_file, 'r') as fp:
@@ -24,22 +52,18 @@ with open(source_file, 'r') as fp:
         row['words'] = []
         sentence = row['sentence']['zh']
         words = jieba.cut(sentence)
+        
         for word in words:
-            if word in ['？', '。', '！']:
+            if word in ['？', '。', '！', ',']:
                 continue
-            if word not in definitions.keys():
-                for character in word:
-                    if character not in definitions.keys():
-                        continue
-                    row['words'].append({
-                        'zh': character,
-                        'en': definitions[character]['translations']['en']
-                    })
-                continue
-            row['words'].append({
-                'zh': word,
-                'en': definitions[word]['translations']['en']
-                })
+            all_defs = get_def_for_word(word, definitions) 
+            for this_def in all_defs:
+                if this_def['zh'] in [x['zh'] for x in row['words']]:
+                    continue
+                row['words'].append(this_def)
+            if len(all_defs) == 0:
+                row['words'].append({'zh': word, 'en': 'Not in word list.'})
+
 
 with open('data/parsed-hsk1-sentences-with-defs.json', 'w', encoding='utf8') as fp:
     json.dump(data, fp, ensure_ascii=False, indent=4)
